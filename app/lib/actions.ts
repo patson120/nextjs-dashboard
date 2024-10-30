1db006084ad4441928c0e41925e2c14adafc12f1
patson120 <patrick1kenne@gmail.com>
2024-10-30 10:33:04 +0100

    Affichage des messages d'erreurs dans la page d'erreur

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,10 +61,12 @@ export async function updateInvoice(id: string, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
+    // throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
-    revalidatePath('/dashboard/invoices');
 }
